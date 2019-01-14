764fd15a9a2de8b9c5d62ae72faf35bf0705adfa
x9mars <x9mars@qq.com>
2019-01-14 15:56:47 +0800

    新增个更新脚本

---
+++ pull.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+git pull -v --progress  "origin" 
+                                                                                                                                                                                                 
